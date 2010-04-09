4bac798cf63116bce5a3df98527d4fe57566d299
Marc Joliet <marcec@gmx.de>
2010-04-09 07:38:22 +0000

    mDirac: change multichannel handling, small changes.
    Made several small changes to mDirac and one larger change: to avoid the problem
    of too many channels causing memory problems (since there were as many dirac
    instances as there were channels), we now create/destroy a dirac instance "on
    the fly" for each channel.  The resulting overhead seems to be insignificantly
    small (tested on Linux only, though).

--- mDirac.c
+++ mDirac.c
@@ -7,7 +7,7 @@ typedef struct {
     double*    dirac_input_data;
     float      dirac_ts_factor;     // time stretch factor
     int        dirac_mode;          // mode (lambda parametr)
-    int        dirac_cur_instance;  // one instance per channel
+    int        dirac_cur_channel;  // one instance per channel
     float      fs;
     int        input_num_channels;
     int        input_num_samples;
@@ -15,8 +15,7 @@ typedef struct {
 } DIRAC_STATE;
 
 /* global variables, so that clear_memory() can reach them */
-DIRAC_STATE dirac_state;
-void** dirac;
+void* dirac;
     
 void initialise_state(DIRAC_STATE *state, int nrhs, const mxArray *prhs[])
 {
@@ -32,10 +31,6 @@ void initialise_state(DIRAC_STATE *state, int nrhs, const mxArray *prhs[])
         state->dirac_input_data   = mxGetPr(prhs[0]);
         state->input_num_samples  = mxGetM(prhs[0]);
         state->input_num_channels = mxGetN(prhs[0]);
-
-        /* TODO: this is an arbitrary limit, think of a more practical one */
-        if( state->input_num_channels > 12 )
-            mexErrMsgTxt("Out of safety reasons max. 12 channels are allowed.");
     }
     else
         mexErrMsgTxt("Only double array are accepted as first argument.");
@@ -45,11 +40,9 @@ void initialise_state(DIRAC_STATE *state, int nrhs, const mxArray *prhs[])
     else
     {
         state->fs = (float)*mxGetPr(prhs[1]);
-        mexPrintf("fs = %f\n", state->fs);
-        if( state->fs != 44100 && state->fs != 48000 ) {
-            mexWarnMsgTxt("Sampling rate can either be 44100 or 48000 Hz. Defaulting to 44100 Hz.");
-            state->fs = 44100.f;
-        }
+        /* mexPrintf("fs = %f\n", state->fs); */
+        if( state->fs != 44100 && state->fs != 48000 )
+            mexErrMsgTxt("Sampling rate can either be 44.1 or 48 kHz, you need to re-sample your data beforehand.");
     }
 
     if( nrhs < 3 )
@@ -73,7 +66,7 @@ void initialise_state(DIRAC_STATE *state, int nrhs, const mxArray *prhs[])
     {
         state->dirac_mode = (int)mxGetScalar(prhs[3]);
         if( state->dirac_mode < 0 || state->dirac_mode > 5 ) {
-            mexWarnMsgTxt("Dirac mode out of bounds: defaulting to mode 3.");
+            mexWarnMsgTxt("Bad Dirac mode: defaulting to mode 3.");
             state->dirac_mode = kDiracLambda3;
         }
     }
@@ -98,7 +91,7 @@ long fill_buffer(float* data, long num_frames, void* dirac_state)
     /* Copy input data to Diracs input buffer. */
     for( i=0; i<N; i++ )
         data[i] = (float)state->dirac_input_data[
-            state->dirac_cur_instance*state->input_num_samples +
+            state->dirac_cur_channel*state->input_num_samples +
             state->cur_sample++];
 
     return N;
@@ -107,10 +100,6 @@ long fill_buffer(float* data, long num_frames, void* dirac_state)
 /* function to clear memory at exit */
 void clear_memory(void)
 {
-    int i;
-    for( i=0; i<dirac_state.input_num_channels; i++ )
-        if( !dirac[i] )
-            DiracDestroy(dirac[i]);
     if( !dirac )
         free(dirac);
 }
@@ -120,57 +109,54 @@ void mexFunction(int nlhs, mxArray *plhs[],
 {
     /* various initiations */
 
+    DIRAC_STATE dirac_state;
     int i, j; // counters in for-loops
     long new_size;
     const long num_frames=8192;
     double* output;
     float* dirac_output;
-    mexAtExit(&clear_memory);
 
+    mexAtExit(&clear_memory);
     initialise_state(&dirac_state, nrhs, prhs);
 
-    /* initiate one Dirac instance per channel */
-
-    dirac = (void**)mxMalloc(dirac_state.input_num_channels*sizeof(void*));
+    dirac = (void*)mxMalloc(dirac_state.input_num_channels*sizeof(void));
     if( !dirac )
         mexErrMsgTxt("Could not create Dirac object array, aborting.\n");
 
-    /* Create one dirac instance per channel beforehand, instead of constantly
-     * creating/destroying in the second for-loop. */
-    for( i=0; i<dirac_state.input_num_channels; i++ )
-    {
-        dirac[i] = DiracCreateInterleaved(dirac_state.dirac_mode,
-                kDiracQualityPreview, 1, dirac_state.fs,
-                &fill_buffer);
-        if( !dirac[i] )
-            mexErrMsgTxt("Could not create Dirac object, aborting.\n");
-
-        /* set the stretch/shift factors */
-        DiracSetProperty(kDiracPropertyTimeFactor, dirac_state.dirac_ts_factor, dirac[i]);
-        DiracSetProperty(kDiracPropertyPitchFactor, 1.0, dirac[i]);
-        DiracSetProperty(kDiracPropertyFormantFactor, 1.0, dirac[i]);
-    }
-
     new_size = (int)ceilf(dirac_state.dirac_ts_factor * dirac_state.input_num_samples);
-    plhs[0]            = mxCreateDoubleMatrix((mwSize)new_size, (mwSize)dirac_state.input_num_channels, mxREAL);
-    output     = mxGetPr(plhs[0]);
+    plhs[0]  = mxCreateDoubleMatrix((mwSize)new_size, (mwSize)dirac_state.input_num_channels, mxREAL);
+    output   = mxGetPr(plhs[0]);
     mexPrintf("Output will be %ix%i Matrix\n", mxGetM(plhs[0]), mxGetN(plhs[0]));
 
     dirac_output = (float*)mxCalloc(num_frames, sizeof(float));
-    if( dirac_output == NULL )
+    if( !dirac_output )
         mexErrMsgTxt("mxCalloc dirac_output failed!");
 
     /* start processing */
     for( i=0; i<dirac_state.input_num_channels; i++ )
     {
         long ret=0, total_size=0;
-        dirac_state.dirac_cur_instance = i;
+        dirac_state.dirac_cur_channel = i;
+
+        /* create a new Dirac instance every channel, to prevent the necessity
+         * of an arbitrary limit on the number of channels; the overhead
+         * shouldn't be too large for normal audio signals */
+        dirac = DiracCreateInterleaved(dirac_state.dirac_mode,
+                kDiracQualityPreview, 1, dirac_state.fs,
+                &fill_buffer);
+        if( !dirac )
+            mexErrMsgTxt("Could not create Dirac object, aborting.\n");
+
+        /* set the stretch/shift factors */
+        DiracSetProperty(kDiracPropertyTimeFactor, dirac_state.dirac_ts_factor, dirac);
+        DiracSetProperty(kDiracPropertyPitchFactor, 1.0, dirac);
+        DiracSetProperty(kDiracPropertyFormantFactor, 1.0, dirac);
 
         do {
             int correction_factor=0;
 
             ret = DiracProcessInterleaved(dirac_output, num_frames,
-                    (void*)&dirac_state, dirac[i]);
+                    (void*)&dirac_state, dirac);
 
             total_size += ret;
             /* correction factor, so that we don't attempt to read more than
@@ -183,6 +169,8 @@ void mexFunction(int nlhs, mxArray *plhs[],
                 output[i*new_size + total_size-ret+j] = (double)dirac_output[j];
         }
         while( ret > 0 );
+
+        DiracDestroy(dirac);
     }
 
     mexPrintf("Done...\n");
