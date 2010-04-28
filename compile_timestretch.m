79ff42bc3cb0b1dc3ff1e1d96c22f5dd4659a34c
Marc Joliet <marcec@gmx.de>
2010-04-28 14:03:22 +0000

    Expand compile_timestretch.m to work on Linux and Mac.
    As an easy to use alternative to the full-fledged SCons build system, there is
    now an m-File that compiles via the mex command. While it is much easier to use
    and doesn't require additional software, it doesn't offer full control over the
    system toolchain, which is the goal of the SCons build system.

--- compile_timestretch.m
+++ compile_timestretch.m
@@ -1,3 +1,13 @@
-mex -LWin -lDiracLE -Iinclude mDirac.c
+% Simple script for compiling mDirac. Afterward compilation a test
+% script is run.
 
+if strcmp(computer, 'PCWIN') || strcmp(computer, 'PCWIN64')
+    mex -LWin -lDiracLE -Iinclude mDirac.c
+elseif strcmp(computer, 'GLNX86') || strcmp(computer, 'GLNXA64')
+    mex -LLinux -lDirac -Iinclude mDirac.c
+elseif strcmp(computer, 'MACI') || strcmp(computer, 'MACI')
+    mex -LMac -lDiracLE -Iinclude mDirac.c
+end
+
+% run test script
 test_dirac
