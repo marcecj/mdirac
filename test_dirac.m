ce2f2014061e8613dae7721e0ee026ad0a93041a
Marc Joliet <marcec@gmx.de>
2010-04-29 15:01:18 +0000

    Test TimeStretchDirac.m more, make small changes.

--- test_dirac.m
+++ test_dirac.m
@@ -8,8 +8,7 @@ close all;
 %
 
 [input, fs] = wavread('test.wav');
-output = mDirac(input,44100,1.5,3);
-% output = mDirac(input,fs,1.5,3);
+output      = mDirac(input,fs,1.5,3);
 
 figure;
 subplot(2,1,1);
@@ -26,12 +25,12 @@ plot(input(:,2));
 % correctly for multichannel stretching)
 %
 
-fs = 44100;
+fs      = 44100;
 ts      = 1.5;
 sig_len = 1;
-t_in = linspace(0,sig_len,fs);
-t_out = linspace(0,ceil(sig_len*ts),ceil(fs*ts));
-sig = sin(2*pi*100*t_in)';
+t_in    = linspace(0,sig_len,fs);
+t_out   = linspace(0,ceil(sig_len*ts),ceil(fs*ts));
+sig     = sin(2*pi*100*t_in)';
 
 a = mDirac(sig,fs,ts,1);
 b = mDirac([sig sig sig],fs,ts,3);
@@ -55,9 +54,12 @@ subplot(3,1,3);
 plot(b(:,3)-b(:,1));
 
 %
-% test 3: test TimeStretchDirac wrapper m-File
+% test 3: test TimeStretchDirac wrapper m-file
 %
 
+output2 = TimeStretchDirac(input);
+output2 = TimeStretchDirac(input,fs);
+output2 = TimeStretchDirac(input,fs,1.5);
 output2 = TimeStretchDirac(input,fs,1.5,3);
 
 figure;
@@ -70,6 +72,9 @@ plot(output2(:,2),'r');
 hold on;
 plot(input(:,2));
 
+output3 = TimeStretchDirac('test.wav');
+output3 = TimeStretchDirac('test.wav',fs);
+output3 = TimeStretchDirac('test.wav',fs,1.5);
 output3 = TimeStretchDirac('test.wav',fs,1.5,3);
 
 figure;
