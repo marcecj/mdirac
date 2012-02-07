marcecj/msndfile

SConstruct: add support for the clang compiler.
All that was needed was to only set the --as-needed flag when GCC is used.

SConstruct
@@ -16,6 +16,9 @@ make_msvs         = GetOption('msvs')
 # the mex_builder tool automatically sets various environment variables
 sndfile      = Environment(tools = ['default', 'packaging', ('mex_builder', {'mex': True})])
 
+# print sndfile['CC']
+# sndfile.Replace(CC="clang")
+
 platform     = sndfile['PLATFORM']
 msvs_variant = "Release"
 
@@ -23,8 +26,9 @@ msvs_variant = "Release"
 if platform == "posix":
     sndfile.Append(LIBPATH="Linux",
                    CCFLAGS="-std=c99 -O2 -pedantic -Wall -Wextra -fdump-rtl-expand",
-                   LIBS=["m"],
-                   LINKFLAGS="--as-needed")
+                   LIBS=["m"])
+    if sndfile['CC'] == 'gcc':
+        sndfile.Append(LINKFLAGS="-Wl,--as-needed")
     if matlab_is_32_bits:
         sndfile.Append(CCFLAGS="-m32", LINKFLAGS="-m32",
                        CPPDEFINES="_FILE_OFFSET_BITS=64")

marcecj/mdirac

SConstruct
@@ -16,6 +16,9 @@ make_msvs         = GetOption('msvs')
 # the mex_builder tool automatically sets various environment variables
 dirac        = Environment(tools = ['default', 'packaging', ('mex_builder', {'mex': True})])
 
+# print dirac['CC']
+# dirac.Replace(CC="clang")
+
 platform     = dirac['PLATFORM']
 msvs_variant = "Release"
 
@@ -23,8 +26,9 @@ msvs_variant = "Release"
 if platform == "posix":
     dirac.Append(LIBPATH="Linux",
                  CCFLAGS="-std=c99 -O2 -pedantic -Wall -Wextra -fdump-rtl-expand",
-                 LIBS=["m"],
-                 LINKFLAGS="--as-needed")
+                 LIBS=["m"])
+    if dirac['CC'] == 'gcc':
+        dirac.Append(LINKFLAGS="-Wl,--as-needed")
     if matlab_is_32_bits:
         dirac.Append(CCFLAGS="-m32", LINKFLAGS="-m32")
     dirac_lib   = "Dirac"